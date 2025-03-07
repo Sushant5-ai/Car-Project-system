0c0ee2ca96b18695aec9e0c7d5242091213266a5
Chahat20-ai <chahatklotra61@gmail.com>
2025-03-07 12:06:45 +0530

    create Signup

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import LoginPage from './Pages/LoginPage/LoginPage';
+import SignUp from './Pages/SignUp/SignUp';
 
 function App() {
   return (
+     
+
+      
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
-      </Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path='/SignUp'element={<SignUp/>}/>
+
+        </Routes>
     </BrowserRouter>
   );
 }
